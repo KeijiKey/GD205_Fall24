0a21af2bc263dd2ae7643f6d8b1b70671b1725e5
KeijiKey <zantebarker1@gmail.com>
2024-09-26 00:32:18 -0400

    update

--- Assets/movement/ChessPieces.cs
+++ Assets/movement/ChessPieces.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChessPieces : MonoBehaviour
-//
 {
+public Transform MrX;
+Vector3 startPos;
+//
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-       //transform.position += new Vector3(1f,0f,0f);
+       //transform.position += new Vector3(0f,0f,0f);
 	 if (Input.GetKeyDown(KeyCode.A)) {
 	 transform.position += Vector3.left;
 	}
@@ -30,9 +33,5 @@ public class ChessPieces : MonoBehaviour
 	if (Input.GetKeyDown(KeyCode.W)) {
 	 transform.position += Vector3.forward;
 	}
-
-	if(transform.position == newVector3(0f, 1f, 2f)){
-	}
-	Debug.Log("Game Over");
     }
 }
