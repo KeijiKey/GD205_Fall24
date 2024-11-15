fca1ebf05d2a02aab0da7151f2ce0c506952973b
KeijiKey <zantebarker1@gmail.com>
2024-11-14 19:03:47 -0500

    update files

--- Assets/movement/MovingApplesYeah.cs
+++ Assets/movement/MovingApplesYeah.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class moveDantedamnit : MonoBehaviour
+public class MovingApplesYeah : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -15,11 +15,14 @@ public class moveDantedamnit : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W)){
 	GetComponent<Animator>().SetFloat("forward", 2f);
+		if (Input.GetKey(KeyCode.LeftShift)){
+	GetComponent<Animator>().SetFloat("forward", 4f);
+}
+
 	}
 else {
 	GetComponent<Animator>().SetFloat("forward", 0f);
 }
-
 
     }
 }
